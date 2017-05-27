abc9b6c95747031dd3db7aa0ac7e729ea4b611d4
coolernow <coolernow641@gmail.com>
2017-05-27 11:53:05 -1000

    Added conditionals for chat bot functionality
    at least 57% less fickle

---
+++ chat-bot/app.js
@@ -0,0 +1,8 @@
+function chat(){
+	var textBoxValue = document.getElementById('dee').value;
+	if (textBoxValue == "hello chap") {
+		console.log('sAIEIEIEIEIE')
+		document.getElementById("Bee").innerHTML = "EXCUSE ME BUT DID YOU JUST ASSUME MY GREETING? BLOCKED!\n";
+	
+	}
+}
